99ddb4c70e926635863c3761d3060327a93e93d6
DESKTOP-TIBM4RV\ACER <minhtkhe172197@fpt.edu.vn>
2024-10-30 07:54:34 +0700

    làm lại homepage + bug phần category

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -3,18 +3,44 @@ package com.example.bookstore;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.bookstore.fragments.HomeFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.activity_main);
 
+        homeFragment = new HomeFragment();
+        loadFragment(homeFragment);
+
+
+
+    }
+
+    private void loadFragment(Fragment homeFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.home_container,homeFragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/bookstore/fragments/HomeFragment.java
@@ -0,0 +1,91 @@
+package com.example.bookstore.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.bookstore.R;
+import com.example.bookstore.adaptes.CategoryAdapter;
+import com.example.bookstore.models.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HomeFragment extends Fragment {
+
+    RecyclerView catRecyclerview;
+
+    //Category recyclerview
+
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+
+    //FireStore
+    FirebaseFirestore db ;
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root =  inflater.inflate(R.layout.fragment_home, container, false);
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
+
+        //image slider
+        ImageSlider imageSlider = root.findViewById(R.id.image_slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.banner1,"Banner 1", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.banner2,"Banner 2", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.banner3,"Banner 3", ScaleTypes.CENTER_CROP));
+
+        imageSlider.setImageList(slideModels);
+
+        //Category
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getActivity(),categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+
+        db.collection( "Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                        }
+                    }
+                });
+
+        return  root;
+    }
+}
