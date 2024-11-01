eddab992ba24092afe24da73a2f89fcb1b20cb7e
DESKTOP-TIBM4RV\ACER <minhtkhe172197@fpt.edu.vn>
2024-11-02 05:08:50 +0700

    Tiep tuc phan home page

--- app/src/main/java/com/example/bookstore/fragments/HomeFragment.java
+++ app/src/main/java/com/example/bookstore/fragments/HomeFragment.java
@@ -10,13 +10,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.bookstore.R;
 import com.example.bookstore.adaptes.CategoryAdapter;
+import com.example.bookstore.adaptes.NewProductsAdapter;
 import com.example.bookstore.models.CategoryModel;
+import com.example.bookstore.models.NewProductsModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
@@ -30,12 +33,17 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView catRecyclerview;
+    RecyclerView catRecyclerview,newProductsRecyclerView;
 
     //Category recyclerview
 
     CategoryAdapter categoryAdapter;
     List<CategoryModel> categoryModelList;
+
+    //New Product recyclerview
+
+    NewProductsAdapter newProductsAdapter;
+    List<NewProductsModel> newProductsModelList;
 
     //FireStore
     FirebaseFirestore db ;
@@ -51,7 +59,9 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.fragment_home, container, false);
 
+
         catRecyclerview = root.findViewById(R.id.rec_category);
+        newProductsRecyclerView = root.findViewById(R.id.new_product_rec);
 
         db = FirebaseFirestore.getInstance();
 
@@ -80,11 +90,42 @@ public class HomeFragment extends Fragment {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 CategoryModel categoryModel = document.toObject(CategoryModel.class);
                                 categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
                             }
-                            categoryAdapter.notifyDataSetChanged();
+
+                        }else{
+
+                            Toast.makeText(getActivity(), ""+task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+
+        // New Products
+        newProductsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        newProductsModelList = new ArrayList<>();
+        newProductsAdapter = new NewProductsAdapter(getContext(),newProductsModelList);
+        newProductsRecyclerView.setAdapter(newProductsAdapter);
+
+        db.collection("NewProducts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                NewProductsModel newProductsModel = document.toObject(NewProductsModel.class);
+                                newProductsModelList.add(newProductsModel);
+                                newProductsAdapter.notifyDataSetChanged();
+                            }
+
+                        }else{
+
+                            Toast.makeText(getActivity(), ""+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
 
         return  root;
     }
