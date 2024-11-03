77bc3969866c64e342be1c7d39a97927deae3b2e
khanh-tre <khanhnxhe171594@fpt.edu.vn>
2024-11-03 21:40:26 +0700

    change quantity

--- app/src/main/java/com/example/bookstore/activities/DetailedActivity.java
+++ app/src/main/java/com/example/bookstore/activities/DetailedActivity.java
@@ -28,10 +28,11 @@ import java.util.HashMap;
 public class DetailedActivity extends AppCompatActivity {
 
     ImageView detailedImg;
-    TextView rating, name, description, price;
+    TextView rating, name, description, price, quantity;
     Button addToCart, buyNow;
     ImageView addItems, removeItems;
-
+    int totalQuantity = 1;
+    int totalPrice = 0;
     //New Products
     NewProductsModel newProductsModel = null;
 
@@ -61,6 +62,7 @@ public class DetailedActivity extends AppCompatActivity {
         }
 
         detailedImg = findViewById(R.id.detailed_img);
+        quantity = findViewById(R.id.quantity);
         name = findViewById(R.id.detailed_name);
         rating = findViewById(R.id.rating);
         description = findViewById(R.id.detailed_desc);
@@ -78,6 +80,8 @@ public class DetailedActivity extends AppCompatActivity {
             description.setText(newProductsModel.getDescription());
             price.setText(String.valueOf(newProductsModel.getPrice()));
             name.setText(newProductsModel.getName());
+
+            totalPrice = newProductsModel.getPrice() * totalQuantity;
         }
         //Popular Products
         if (popularProductsModel != null) {
@@ -87,6 +91,7 @@ public class DetailedActivity extends AppCompatActivity {
             description.setText(popularProductsModel.getDescription());
             price.setText(String.valueOf(popularProductsModel.getPrice()));
             name.setText(popularProductsModel.getName());
+            totalPrice = popularProductsModel.getPrice() * totalQuantity;
         }
 
         //Show ALl Products
@@ -97,12 +102,43 @@ public class DetailedActivity extends AppCompatActivity {
             description.setText(showAllModel.getDescription());
             price.setText(String.valueOf(showAllModel.getPrice()));
             name.setText(showAllModel.getName());
+            totalPrice = showAllModel.getPrice() * totalQuantity;
         }
 
         addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 addToCart();
+            }
+        });
+
+        addItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(totalQuantity<10){
+                    totalQuantity++;
+                    quantity.setText(String.valueOf(totalQuantity));
+
+                    if(newProductsModel != null){
+                        totalPrice = newProductsModel.getPrice()*totalQuantity;
+                    }
+                    if(popularProductsModel != null){
+                        totalPrice = popularProductsModel.getPrice()*totalQuantity;
+                    }
+                    if(showAllModel != null){
+                        totalPrice = showAllModel.getPrice()* totalQuantity;
+                    }
+                }
+            }
+        });
+
+        removeItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(totalQuantity>1){
+                    totalQuantity--;
+                    quantity.setText(String.valueOf(totalQuantity));
+                }
             }
         });
     }
@@ -123,6 +159,8 @@ public class DetailedActivity extends AppCompatActivity {
         cartMap.put("productPrice",price.getText().toString());
         cartMap.put("currentTime",saveCurrentTime);
         cartMap.put("currentDate",saveCurrentDate);
+        cartMap.put("totalQuantity",quantity.getText().toString());
+        cartMap.put("totalPrice",totalPrice);
 
         firestore.collection("AddtoCart").document(auth.getCurrentUser().getUid())
                 .collection("User").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
